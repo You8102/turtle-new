7dc6f025aa9c4d358f82547d6542d52585b46370
You8102 <yuko.tonami73@gmail.com>
2023-09-11 21:45:52 +0900

    拾い物

--- scripts/talker.py
+++ scripts/talker.py
@@ -5,9 +5,9 @@ from geometry_msgs.msg import Twist
 if __name__ =="__main__":
 
     rospy.init_node("talker_node")
-    
+    pub = rospy.Publisher("T", Twist, queue_size=10)
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-        talker.publish()
+        pub.publish(msg)
         rate.sleep()
