67d1aa44d07c7dfcf3cd3f80a9fa9235021962f5
You8102 <yuko.tonami73@gmail.com>
2023-09-12 14:31:41 +0900

    とりあえず動いた

---
+++ scripts/listner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+
+if __name__ =="__main__":
+    rospy.init_node("listener_node")
+    rospy.spin()
--- scripts/talker.py
+++ scripts/talker.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 import rospy
+import sys
 from geometry_msgs.msg import Twist
 
 if __name__ =="__main__":
 
-    rospy.init_node("talker_node")
-    pub = rospy.Publisher("T", Twist, queue_size=10)
+    rospy.init_node("kame")
+    pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
     rate = rospy.Rate(10)
+    vel = Twist()
+    
+
 
     while not rospy.is_shutdown():
-        pub.publish(msg)
+        vel.linear.x = 1
+        vel.linear.y = 0
+        vel.linear.z = 0
+        vel.angular.x = 0
+        vel.angular.y = 0
+        vel.angular.z = 1
+        pub.publish(vel)
+        rospy.loginfo(vel)
         rate.sleep()
